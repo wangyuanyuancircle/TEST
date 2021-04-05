32d5c4544a471a649e1586e9ec00441f15ac5035
xiguapi1 <1483330793@qq.com>
2021-04-05 17:49:01 +0800

    测试使用

---
+++ src/views/vant.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    硝烟四起
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell title="选择单个日期" :value="date" @click="show = true" />
+    <van-calendar v-model="show" @confirm="onConfirm" />
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import { Button, Calendar, Cell} from "vant";
+
+Vue.use(Button);
+Vue.use(Cell);
+Vue.use(Calendar);
+export default {
+  data() {
+    return {
+      date: '',
+      show: false,
+    };
+  },
+  methods: {
+    formatDate(date) {
+      return `${date.getMonth() + 1}/${date.getDate()}`;
+    },
+    onConfirm(date) {
+      this.show = false;
+      this.date = this.formatDate(date);
+    },
+  },
+};
+</script>
